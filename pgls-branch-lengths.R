32320b620b4954e96c8e3a5110a808bc2c941783
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-06-26 14:55:08 +0200

    Include PGLM; truncate simulated Y at zero

--- pgls-branch-lengths.R
+++ pgls-branch-lengths.R
@@ -3,8 +3,9 @@ library(phangorn)
 library(nlme)
 library(ggplot2)
 library(gridExtra)
+library(phylolm)
 
-# explores the power of PGLS to find correlations under some awkward conditions
+# explores the power of PGLS and PGLM to find correlations under some awkward conditions
 # (i) the variables x and y are not well-behaved: x is binary and y discrete, and the rate of decrease of y depends on x
 # (ii) x is not observed perfectly: p01 is the probability of seeing a 1 when x=0 and p10 the probability of seeing a 0 when x=1
 #      [note for things like parasitism I think p01=0]
@@ -95,37 +96,50 @@ for(tree.size in 2**c(6)) {
               #g.1 = ggplot(df, aes(x=factor(obs.x), y=y)) + geom_jitter(width=0.25) + geom_boxplot(alpha = 0.2)
               #g.2 = ggplot(tip.df, aes(x=factor(obs.x), y=y)) + geom_jitter(width=0.25) + geom_boxplot(alpha = 0.2)
               #grid.arrange(g.1, g.2, nrow=1)
-
+              tip.df$y[tip.df$y < 0] = 0
+              
               ####### basic, naive correlation
               basic.test = cor.test(tip.df$obs.x, tip.df$y)
               basic.pval = basic.test$p.value
 
-              ####### PGLS under a Brownian model for correlations (respecting branch lengths)
-              my.correlation = corBrownian(phy=my.tree, form=~label)
-              if(all(tip.df$obs.x == 0) | all(tip.df$obs.x == 1)) { 
-                pgls.pval = NA 
+              if(all(tip.df$obs.x == 0) | all(tip.df$obs.x == 1) | all(tip.df$y == 0) |
+                 (length(which(tip.df$y[tip.df$obs.x==1] > 0)) < 2) |
+                 (length(which(tip.df$y[tip.df$obs.x==0] > 0)) < 2)) { 
+                pgls.pval = pgls.nb.pval = pglm.pval = pglm.nb.pval = NA
               } else {
+                ####### PGLS under a Brownian model for correlations (respecting branch lengths)
+                my.correlation = corBrownian(phy=my.tree, form=~label)
+         
                 pgls.mod = gls(y~obs.x, correlation=my.correlation, data=tip.df, method="ML", na.action=na.omit)
                 gls.mod = gls(y~obs.x, data=tip.df, method="ML", na.action=na.omit)
                 pgls.coef = coef(pgls.mod)[2]
                 pgls.pval = anova(pgls.mod)$'p-value'[2]
                 pgls.pval
-              }
-  
-              ####### PGLS under a Brownian model for correlations (ignoring branch lengths)
-              my.tree.nb = my.tree
-              my.tree.nb$edge.length = rep(mean(my.tree$edge.length), length(my.tree$edge))
-              my.correlation.nb = corBrownian(phy=my.tree.nb, form=~label)
-              if(all(tip.df$obs.x == 0) | all(tip.df$obs.x == 1)) { 
+             
+                ####### PGLS under a Brownian model for correlations (ignoring branch lengths)
+                my.tree.nb = my.tree
+                my.tree.nb$edge.length = rep(mean(my.tree$edge.length), length(my.tree$edge))
+                my.correlation.nb = corBrownian(phy=my.tree.nb, form=~label)
+      
                 pgls.nb.pval = NA 
-              } else {
                 pgls.nb.mod = gls(y~obs.x, correlation=my.correlation.nb, data=tip.df, method="ML", na.action=na.omit)
                 gls.nb.mod = gls(y~obs.x, data=tip.df, method="ML", na.action=na.omit)
                 pgls.nb.coef = coef(pgls.nb.mod)[2]
                 pgls.nb.pval = anova(pgls.nb.mod)$'p-value'[2]
                 pgls.nb.pval
+                
+                ####### PGLM with branch lengths
+                rownames(tip.df) = tip.df$label
+                pglm = phyloglm(y~obs.x, tip.df, my.tree, method="poisson_GEE")
+                pglm.coef = summary(pglm)$coefficients[2,1]
+                pglm.pval = summary(pglm)$coefficients[2,4]
+              
+                ####### PGLM without branch lengths
+                pglm.nb = phyloglm(y~obs.x, tip.df, my.tree.nb, method="poisson_GEE")
+                pglm.nb.coef = summary(pglm.nb)$coefficients[2,1]
+                pglm.nb.pval = summary(pglm.nb)$coefficients[2,4]
+
               }
-            
               # debug stop criterion
               #if(pgls.pval < 1e-3 & model.correlated == 0) { stop() }
               
@@ -133,7 +147,7 @@ for(tree.size in 2**c(6)) {
               pvals = rbind(pvals, data.frame(tree.size=tree.size, model.correlated=model.correlated, 
                                               mean.events=mean.events, death=death,
                                               p01=p01, p10=p10, expt=expt, basic.pval=basic.pval, pgls.pval=pgls.pval, 
-                                              pgls.nb.pval=pgls.nb.pval))
+                                              pgls.nb.pval=pgls.nb.pval, pglm.pval=pglm.pval, pglm.nb.pval=pglm.nb.pval))
             }
           }
         }
@@ -176,11 +190,27 @@ g.3 =  ggplot(pvals[pvals$p01==0,], aes(x=factor(model.correlated), y=log10(-log
   facet_grid(mean.events~death)+
   theme_light() + xlab("True effect") + ylab("log(-log(p)) naive") + labs(color="Obs error")
 
+# PGLS with branch lengths
+g.4 = ggplot(pvals[pvals$p01==0,], aes(x=factor(model.correlated), y=log10(-log10(pglm.pval)), color=noise.label)) + 
+  geom_boxplot() + geom_hline(yintercept=log10(-log10(0.05)), color="#888888")+ 
+  geom_vline(xintercept=1.5, color="#888888")+
+  geom_text(data=label.text,aes(x=x,y=y,label=label),color="#888888",size=3) +
+  facet_grid(mean.events~death) +
+  theme_light() + xlab("True effect") + ylab("log(-log(p)) PGLM") + labs(color="Obs error")
+
+# PGLM without branch lengths
+g.5 = ggplot(pvals[pvals$p01==0,], aes(x=factor(model.correlated), y=log10(-log10(pglm.nb.pval)), color=noise.label)) + 
+  geom_boxplot() + geom_hline(yintercept=log10(-log10(0.05)), color="#888888")+ 
+  geom_vline(xintercept=1.5, color="#888888")+
+  geom_text(data=label.text,aes(x=x,y=y,label=label),color="#888888",size=3) +
+  facet_grid(mean.events~death) +
+  theme_light() + xlab("True effect") + ylab("log(-log(p)) PGLM w/o branches") + labs(color="Obs error")
+
 # plot all
-grid.arrange(g.1, g.2, g.3, nrow=3)
+grid.arrange(g.1, g.2, g.3, g.4, g.5, nrow=3)
 sf = 2
 png("pgls-branches.png", width=600*sf, height=800*sf, res=72*sf)
-grid.arrange(g.1, g.2, g.3, nrow=3)
+grid.arrange(g.1, g.2, g.3, g.4, g.5, nrow=3)
 dev.off()
 
 # summarise info on observation statistics
